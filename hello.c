c53d89abc87d77cfa2ea95361231b7db0174ffed
fboris <fantasyboris@gmail.com>
2013-10-09 00:34:53 +0800

    Replace the file related calls with UNIX-like system calls
    In this commit, I remove all of file related calls such as fopen and
    fclose. Use Unix-like system call(e.g. open, close, read)instead of
    them.

--- hello.c
+++ hello.c
@@ -1,18 +1,30 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <inttypes.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define FILE_PATH "./myLog.txt"
 
 int main(void)
 {
-	char input[80];
-	FILE * myLog;
+	int fd = open( FILE_PATH, O_RDONLY);
 	char ch;
-	puts("You called a semi-host. enter y to write a log");
-	puts("reading file from workspace....\n");
-	myLog = fopen("myLog", "a");//append file
-	puts("Please write some log!\n");
-	gets(input);
-	fprintf(myLog ,"%s\n", input);
-	fclose(myLog);
-
+	int IsDone = 1;
+	int byte_len;
+	while(IsDone){
+		
+		byte_len = read( fd, &ch, 1);
+		printf("%c", ch);
+		if(byte_len <= 0){
+			printf("find the end of file!");
+			break;
+		}
+	
+	}
+	
+	close(fd);
+	
+	
 	return 0;
 }
