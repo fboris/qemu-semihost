061987324afbceb7f7e3640545b9133e944d70c4
fboris <fantasyboris@gmail.com>
2013-10-09 15:11:25 +0800

    Use array buffer instead of one byte char. Add file open error handling.
    Use specific size of array to be store file. But if I read large
    file which bigger than buffer, It will have some strange characters that
    aren't exist. Sometimes, terminal also doesn't display some characters in
    file.

--- hello.c
+++ hello.c
@@ -5,24 +5,38 @@
 #include <fcntl.h>
 
 #define FILE_PATH "./myLog.txt"
-
+#define BUFFER_SIZE 100
 int main(void)
 {
 	int fd = open( FILE_PATH, O_RDONLY);
-	char ch;
+	char buff[BUFFER_SIZE];
 	int IsDone = 1;
 	int byte_len;
+	/*error handling*/
+	if( fd == -1){
+		printf("file not found\n");
+		return 0;
+	}
+	else{
+		printf("found file, start reading...");
+	
+	}
+	int counter = 0;
+	/*reading loop*/
 	while(IsDone){
-		
-		byte_len = read( fd, &ch, 1);
-		printf("%c", ch);
+		byte_len = read( fd, buff, BUFFER_SIZE);
 		if(byte_len <= 0){
 			printf("find the end of file!");
 			break;
 		}
-	
+		else{
+			printf("%s", buff);
+		}
+		
 	}
 	
+
+
 	close(fd);
 	
 	
